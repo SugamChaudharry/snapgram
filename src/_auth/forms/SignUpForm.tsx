21ff8d621bf8607bf573c2b1aea95f008f4167a2
SugamChaudharry <nextgenachieverss@gmail.com>
2024-07-06 13:31:52 +0530

    fix: Error:: type declarations.ts(2307)

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
 
 const SighUpForm = () => {
   return (
-    <div>SighUpForm</div>
+    <div>
+    <Button>Click me</Button>
+  </div>
   )
 }
 
